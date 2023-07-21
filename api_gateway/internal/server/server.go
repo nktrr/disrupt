0eeb10c372652d1ddb5ea1be82f0b2b162033d05
nktrr <nekitrudnevv@gmail.com>
2023-07-21 20:23:50 +0300

    add kafka pkg

--- api_gateway/internal/server/server.go
+++ api_gateway/internal/server/server.go
@@ -1,12 +1,34 @@
 package server
 
 import (
+	"context"
 	"disrupt/api_gateway/config"
-	"log"
+	"disrupt/pkg/kafka"
+	"disrupt/pkg/logger"
+	"github.com/labstack/echo"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 type server struct {
-	log  log.Logger
+	log  logger.Logger
 	cfg  *config.Config
 	echo *echo.Echo
+}
+
+func NewServer(log logger.Logger, cfg *config.Config) *server {
+	return &server{
+		log:  log,
+		cfg:  cfg,
+		echo: echo.New(),
+	}
+}
+
+func (s *server) Run() error {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+	defer cancel()
+	kafkaProducer := kafka.NewProducer(s.log, s.cfg.Kafka.Brokers)
+	defer kafkaProducer.Close()
+	
 }
